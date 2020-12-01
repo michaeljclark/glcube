c991bc70ef8dcfc0ff131fa0f61030faf049a08b
Michael Clark <michaeljclark@mac.com>
2020-12-02 08:21:16 +1300

    glcube: move rotation into state structure and update keyboard navigation

--- src/gl2_cube.c
+++ src/gl2_cube.c
@@ -33,21 +33,21 @@ typedef struct model_object {
 
 typedef struct zoom_state {
     float zoom;
-    vec2 mouse_pos;
-    vec2 origin;
+    vec2f mouse_pos;
+    vec2f origin;
+    vec3f rotation;
 } zoom_state_t;
 
 static const char* frag_shader_filename = "shaders/cube.fsh";
 static const char* vert_shader_filename = "shaders/cube.vsh";
 
-static GLfloat view_rotx = 20.f, view_roty = 30.f, view_rotz = 0.f;
 static GLfloat angle = 0.f;
-static bool animate_enable = 0;
+static bool animation = 0;
 static bool debug_vertices = 0;
 static GLuint program;
 static mat4x4 v, p;
 static model_object_t mo[1];
-static zoom_state_t state = { 32.0f }, state_save;
+static zoom_state_t state = { 32.0f, { 0.f }, { 0.f }, { 20.f, 30.f, 0.f } }, state_save;
 static const float min_zoom = 16.0f, max_zoom = 32768.0f;
 static bool mouse_left_drag = false;
 static bool mouse_right_drag = false;
@@ -106,12 +106,12 @@ static void model_object_cube(model_object_t *mo, float s, vec4f col)
     index_buffer_add_primitves(&mo->ib, primitive_topology_quad_strip, 12, idx);
 }
 
-static void model_view_pos(float zoom, float rotx, float roty, float rotz)
+static void model_view_pos(float zoom, vec3f rot)
 {
     mat4x4_translate(v, 0.0, 0.0, zoom);
-    mat4x4_rotate(v, v, 1.0, 0.0, 0.0, (rotx / 180) * M_PI);
-    mat4x4_rotate(v, v, 0.0, 1.0, 0.0, (roty / 180) * M_PI);
-    mat4x4_rotate(v, v, 0.0, 0.0, 1.0, (rotz / 180) * M_PI);
+    mat4x4_rotate(v, v, 1.0, 0.0, 0.0, (rot.x / 180) * M_PI);
+    mat4x4_rotate(v, v, 0.0, 1.0, 0.0, (rot.y / 180) * M_PI);
+    mat4x4_rotate(v, v, 0.0, 0.0, 1.0, (rot.z / 180) * M_PI);
 }
 
 static void model_object_pos(model_object_t *mo,float angle,
@@ -136,8 +136,8 @@ static void draw()
     glClearColor(1.0, 1.0, 1.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    model_view_pos(-state.zoom, view_rotx, view_roty, view_rotz);
-    model_object_pos(&mo[0], angle, state.origin[0]*0.01f, state.origin[1]*0.01f, 0.0);
+    model_view_pos(-state.zoom, state.rotation);
+    model_object_pos(&mo[0], angle, state.origin.x * 0.01f, state.origin.y * 0.01f, 0.0);
     uniform_matrix_4fv("u_model", (const GLfloat *)mo->m);
     uniform_matrix_4fv("u_view", (const GLfloat *)v);
     model_object_draw(&mo[0]);
@@ -151,7 +151,7 @@ static void animate()
     current_time = (float) glfwGetTime();
     delta_time = current_time - last_time;
 
-    if (animate_enable) {
+    if (animation) {
         angle += 100.f * delta_time;
     }
 }
@@ -160,20 +160,19 @@ void key( GLFWwindow* window, int k, int s, int action, int mods )
 {
     if( action != GLFW_PRESS ) return;
 
-    float modm = (mods & GLFW_MOD_SHIFT) ? 1.0f : -1.0f;
+    float shiftz = (mods & GLFW_MOD_SHIFT ? -1.0 : 1.0);
 
     switch (k) {
-    case GLFW_KEY_A:     animate_enable = !animate_enable; break;
-    case GLFW_KEY_Z:     view_rotz += 5.0 * modm; break;
-    case GLFW_KEY_UP:    view_rotx += 5.0; break;
-    case GLFW_KEY_DOWN:  view_rotx -= 5.0; break;
-    case GLFW_KEY_LEFT:  view_roty += 5.0; break;
-    case GLFW_KEY_RIGHT: view_roty -= 5.0; break;
     case GLFW_KEY_ESCAPE:
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-        break;
-    default:
-        return;
+    case GLFW_KEY_Q: glfwSetWindowShouldClose(window, GLFW_TRUE); break;
+    case GLFW_KEY_X: animation = !animation; break;
+    case GLFW_KEY_Z: state.rotation.z += 5.0 * shiftz; break;
+    case GLFW_KEY_C: state.zoom += 5.0 * shiftz; break;
+    case GLFW_KEY_W: state.rotation.x += 5.0; break;
+    case GLFW_KEY_S: state.rotation.x -= 5.0; break;
+    case GLFW_KEY_A: state.rotation.y += 5.0; break;
+    case GLFW_KEY_D: state.rotation.y -= 5.0; break;
+    default: return;
     }
 }
 
@@ -193,12 +192,12 @@ static void scroll(GLFWwindow* window, double xoffset, double yoffset)
     float quantum = state.zoom / 16.0f;
     float ratio = 1.0f + (float)quantum / (float)state.zoom;
     if (yoffset < 0 && state.zoom < max_zoom) {
-        state.origin[0] *= ratio;
-        state.origin[1] *= ratio;
+        state.origin.x *= ratio;
+        state.origin.y *= ratio;
         state.zoom += quantum;
     } else if (yoffset > 0 && state.zoom > min_zoom) {
-        state.origin[0] /= ratio;
-        state.origin[1] /= ratio;
+        state.origin.x /= ratio;
+        state.origin.y /= ratio;
         state.zoom -= quantum;
     }
 }
@@ -219,23 +218,23 @@ static void mouse_button(GLFWwindow* window, int button, int action, int mods)
 
 static void cursor_position(GLFWwindow* window, double xpos, double ypos)
 {
-    state.mouse_pos[0] = xpos;
-    state.mouse_pos[1] = ypos;
+    state.mouse_pos.x = xpos;
+    state.mouse_pos.y = ypos;
 
     if (mouse_left_drag) {
-        state.origin[0] += state.mouse_pos[0] - state_save.mouse_pos[0];
-        state.origin[1] += state.mouse_pos[1] - state_save.mouse_pos[1];
-        state_save.mouse_pos[0] = state.mouse_pos[0];
-        state_save.mouse_pos[1] = state.mouse_pos[1];
+        state.origin.x += state.mouse_pos.x - state_save.mouse_pos.x;
+        state.origin.y += state.mouse_pos.y - state_save.mouse_pos.y;
+        state_save.mouse_pos.x = state.mouse_pos.x;
+        state_save.mouse_pos.y = state.mouse_pos.y;
     }
     if (mouse_right_drag) {
-        float delta0 = state.mouse_pos[0] - state_save.mouse_pos[0];
-        float delta1 = state.mouse_pos[1] - state_save.mouse_pos[1];
+        float delta0 = state.mouse_pos.x - state_save.mouse_pos.x;
+        float delta1 = state.mouse_pos.y - state_save.mouse_pos.y;
         float zoom = state_save.zoom * powf(65.0f/64.0f,(float)-delta1);
         if (zoom != state.zoom && zoom > min_zoom && zoom < max_zoom) {
             state.zoom = zoom;
-            state.origin[0] = (state.origin[0] * (zoom / state.zoom));
-            state.origin[1] = (state.origin[1] * (zoom / state.zoom));
+            state.origin.x = (state.origin.x * (zoom / state.zoom));
+            state.origin.y = (state.origin.y * (zoom / state.zoom));
         }
     }
 }
